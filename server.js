34658dfe2365213fa6f4fe3b0f1060f606bb6e40
Hlanganani <127399757+Msawekho@users.noreply.github.com>
2024-11-05 11:46:15 +0200

    Added server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`To-Do List app running at http://localhost:${port}`);
+});
